22b3dd5abd8b26b979fbe28227f2710336416f38
rafaelwoodi <rafael.domingues@ilegra.com>
2013-08-19 17:13:24 -0300

    Criado parâmetro Senha na classe Usuarios.

--- ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
+++ ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
@@ -4,6 +4,7 @@ public class Usuarios {
 	private int cod_usuario;
     private String login;
 	private String nome;
+	private String senha;
 	
 	public int getCod_usuario() {
 		return cod_usuario;
@@ -22,6 +23,12 @@ public class Usuarios {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 
