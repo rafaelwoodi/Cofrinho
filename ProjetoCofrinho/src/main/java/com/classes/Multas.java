98488127943ba3100094176a015b1c5bcc6110b7
Jeferson Angeli Ghisio <jeferson.ghisio@ilegra.com>
2013-08-19 17:09:11 -0300

    Adicionado gets e sets na classe multas

--- ProjetoCofrinho/src/main/java/com/classes/Multas.java
+++ ProjetoCofrinho/src/main/java/com/classes/Multas.java
@@ -1,5 +1,26 @@
 package com.classes;
 
 public class Multas {
-
+  private Integer codMulta;
+  private Integer codTipoMulta;
+  private double valor;
+  
+public Integer getCodMulta() {
+	return codMulta;
+}
+public void setCodMulta(Integer codMulta) {
+	this.codMulta = codMulta;
+}
+public Integer getCodTipoMulta() {
+	return codTipoMulta;
+}
+public void setCodTipoMulta(Integer codTipoMulta) {
+	this.codTipoMulta = codTipoMulta;
+}
+public double getValor() {
+	return valor;
+}
+public void setValor(double valor) {
+	this.valor = valor;
+} 
 }
