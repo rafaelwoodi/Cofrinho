3982bc90d8873532abbdcf39ebf0a4bb290fab76
rafaelwoodi <rafael.domingues@ilegra.com>
2013-08-19 17:04:47 -0300

    Adicionado Getters and Setters na classe Usuário.

--- ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
+++ ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
@@ -1,5 +1,28 @@
 package com.classes;
 
 public class Usuarios {
+	private int cod_usuario;
+    private String login;
+	private String nome;
+	
+	public int getCod_usuario() {
+		return cod_usuario;
+	}
+	public void setCod_usuario(int cod_usuario) {
+		this.cod_usuario = cod_usuario;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 
 }
