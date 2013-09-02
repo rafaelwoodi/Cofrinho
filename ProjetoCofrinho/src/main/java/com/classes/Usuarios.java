c0669c56b19192830164b6ae125dd333ae3b56d6
Jeferson Angeli Ghisio <jeferson.ghisio@ilegra.com>
2013-09-01 21:54:00 -0300

    Colocado algumas informações do hibernate

--- ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
+++ ProjetoCofrinho/src/main/java/com/classes/Usuarios.java
@@ -1,10 +1,38 @@
 package com.classes;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (schema="cofrinho", name="usuarios")
 public class Usuarios {
-	private int cod_usuario;
+	@Id
+	@GeneratedValue
+	private Integer cod_usuario;	
+	private Integer cod_tipo_usuario;
+	private Integer cod_equipe;
+	
     private String login;
 	private String nome;
 	private String senha;
+	
+	public Integer getCod_tipo_usuario() {
+		return cod_tipo_usuario;
+	}
+	public void setCod_tipo_usuario(Integer cod_tipo_usuario) {
+		this.cod_tipo_usuario = cod_tipo_usuario;
+	}
+	public Integer getCod_equipe() {
+		return cod_equipe;
+	}
+	public void setCod_equipe(Integer cod_equipe) {
+		this.cod_equipe = cod_equipe;
+	}
+	public void setCod_usuario(Integer cod_usuario) {
+		this.cod_usuario = cod_usuario;
+	}	
 	
 	public int getCod_usuario() {
 		return cod_usuario;
@@ -29,7 +57,6 @@ public class Usuarios {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
-	}
-	
+	}	
 
 }
